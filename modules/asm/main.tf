f3debeebbc3e2dab12fb2c4b6455c49ce621a3d4
Zufishan Ali <zufishan.ali@telus.com>
2024-01-17 14:30:36 -0500

    update

--- modules/asm/main.tf
+++ modules/asm/main.tf
@@ -89,7 +89,7 @@ module "asm-services" {
 
 module "asm_install" {
   #source  = "terraform-google-modules/gcloud/google//modules/kubectl-wrapper"
-  source  ="git@github.com:aneeshchulliparambilmohanan/terraform-google-gcloud.git//modules/kubectl-wrapper?ref=v3.3.16"
+  source  ="git@github.com:aneeshchulliparambilmohanan/terraform-google-gcloud.git//modules/kubectl-wrapper?ref=v3.3.17"
   #version = "~> 3.1"
 
   module_depends_on = concat([var.cluster_endpoint], local.additional_depends_on)
